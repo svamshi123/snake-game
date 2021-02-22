24cf882e5ff11f45c7eb7e0908ebbdd46b0653bf
svamshi123 <saivamshiadb@gmail.com>
2021-02-22 15:41:54 +0530

    first build

---
+++ lwc-services.config.js
@@ -0,0 +1,19 @@
+// Find the full example of all available configuration options at
+// https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
+module.exports = {
+    buildDir: './docs',
+    resources: [{ from: 'src/client/resources/', to: 'docs/resources/' }
+
+
+
+
+    ],
+
+    sourceDir: './src/client',
+
+
+    devServer: {
+        proxy: { '/': 'http://localhost:3002' }
+    }
+
+};
